6d369f415b5c8297a028b962c915a742f60ef169
박기현 <kihyun1998@penta.co.kr>
2024-11-04 22:52:29 +0900

    Update: get interval string 개선

--- price_manager.go
+++ price_manager.go
@@ -9,21 +9,30 @@ import (
 	lib "github.com/assist-by/libStruct"
 )
 
+var intervalMap = map[time.Duration]string{
+	time.Minute:         "1m",
+	15 * time.Minute:    "15m",
+	30 * time.Minute:    "30m",
+	time.Hour:           "1h",
+	2 * time.Hour:       "2h",
+	4 * time.Hour:       "4h",
+	6 * time.Hour:       "6h",
+	8 * time.Hour:       "8h",
+	12 * time.Hour:      "12h",
+	24 * time.Hour:      "1d",
+	7 * 24 * time.Hour:  "1w",
+	30 * 24 * time.Hour: "1M",
+}
+
 func nextIntervalStart(now time.Time, interval time.Duration) time.Time {
 	return now.Truncate(interval).Add(interval)
 }
 
 func getIntervalString(interval time.Duration) string {
-	switch interval {
-	case 1 * time.Minute:
-		return "1m"
-	case 15 * time.Minute:
-		return "15m"
-	case 1 * time.Hour:
-		return "1h"
-	default:
-		return "15m"
+	if str, exists := intervalMap[interval]; exists {
+		return str
 	}
+	return "15m" // 기본값
 }
 
 func fetchBTCCandleData(url string) ([]lib.CandleData, error) {
