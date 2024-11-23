19c10af8df595bd41c08e1df91c388543ec4d119
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-11-23 13:33:58 +0900

    Update: future buy 코드 구현

--- futures/order.go
+++ futures/order.go
@@ -59,4 +59,66 @@ func (f *FutureClient) PlaceOrder(order OrderRequest) error {
 	}
 
 	defer resp.Body.Close()
+
+	if order.TakeProfit > 0 {
+		tpParams := url.Values{}
+		tpParams.Add("symbol", order.Symbol)
+		tpParams.Add("side", string(getOppositeOrderSide(order.Side)))
+		tpParams.Add("type", "TAKE_PROFIT_MARKET")
+		tpParams.Add("stopPrice", strconv.FormatFloat(order.TakeProfit, 'f', -1, 64))
+		tpParams.Add("quantity", strconv.FormatFloat(order.Quantity, 'f', -1, 64))
+		tpParams.Add("timestamp", strconv.FormatInt(time.Now().UnixMilli(), 10))
+
+		signature = f.sign(tpParams.Encode())
+		tpParams.Add("signature", signature)
+
+		if err := f.placeStopOrder(tpParams); err != nil {
+			return fmt.Errorf("placing take profit: %w", err)
+		}
+	}
+
+	if order.StopLoss > 0 {
+		slParams := url.Values{}
+		slParams.Add("symbol", order.Symbol)
+		slParams.Add("side", string(getOppositeOrderSide(order.Side)))
+		slParams.Add("type", "STOP_MARKET")
+		slParams.Add("stopPrice", strconv.FormatFloat(order.StopLoss, 'f', -1, 64))
+		slParams.Add("quantity", strconv.FormatFloat(order.Quantity, 'f', -1, 64))
+		slParams.Add("timestamp", strconv.FormatInt(time.Now().UnixMilli(), 10))
+
+		signature = f.sign(slParams.Encode())
+		slParams.Add("signature", signature)
+
+		if err := f.placeStopOrder(slParams); err != nil {
+			return fmt.Errorf("placing stop loss: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (f *FutureClient) placeStopOrder(params url.Values) error {
+	req, err := http.NewRequest("POST", f.BaseURL+"/fapi/v1/order", nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-MBX-APIKEY", f.APIKey)
+	req.URL.RawQuery = params.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	return nil
+}
+
+func getOppositeOrderSide(side OrderSide) OrderSide {
+	if side == BUY {
+		return SELL
+	}
+
+	return BUY
 }
